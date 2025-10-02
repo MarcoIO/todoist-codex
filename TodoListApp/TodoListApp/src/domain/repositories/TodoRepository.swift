a070ec3219d6493d60cb0b4740077c6986ba8dbe
Marco IO <43174474+MarcoIO@users.noreply.github.com>
2025-10-02 12:59:15 +0100

    Remove all

--- TodoListApp/TodoListApp/src/domain/repositories/TodoRepository.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// Defines the contract for accessing and mutating TODO items.
-public protocol TodoRepository {
-    func fetchAll() throws -> [TodoItem]
-    func get(by identifier: UUID) throws -> TodoItem
-    func save(_ item: TodoItem) throws
-    func delete(_ identifier: UUID) throws
-}
